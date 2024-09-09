7f9224d682da2133b6cb012f8ce97fca3d2bcd98
SergeyKhvalev83 <khvalevelbrus@gmail.com>
2024-09-09 17:20:17 -0400

    text align justify about section

--- app/components/AboutMeSection.tsx
+++ app/components/AboutMeSection.tsx
@@ -8,7 +8,7 @@ const AboutMeSection = () => {
   return (
     <section id="about" className={classes}>
       <div className="center">
-        <h2> About </h2>{" "}
+        <h2> About </h2>
         <p>
         Passionate Full Stack Developer with over 4 years of experience in modern web technologies, specializing in building engaging, user-centric applications. Skilled in JavaScript, TypeScript, React, and Redux, I create seamless, dynamic user experiences. My journey in web development is driven by continuous learning, staying ahead of industry trends, and consistently pushing the boundaries of what is possible on the web.
         </p>
